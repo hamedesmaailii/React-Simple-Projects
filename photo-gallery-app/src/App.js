d9be6c9030c4274f5ba58d25118137f6fec578ff
hamedesmaailii <hamed.esmaili.1375@gmail.com>
2024-07-14 17:24:20 +0330

    work on photo gallery app

--- photo-gallery-app/src/App.js
+++ photo-gallery-app/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        const fetchPhotos = async () => {
+            try {
+                const response = await axios.get('https://api.unsplash.com/photos', {
+                    params: {per_page: 10},
+                    headers: {Authorization: }
+                })
+            }
+        }
+    }, []);
+
     return (
         <div className="App">
-            
+
         </div>
     );
 }
