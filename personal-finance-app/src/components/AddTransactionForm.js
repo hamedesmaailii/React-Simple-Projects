c1c4eb4182676be4fc86261998a3f73ccf6961cb
hamedesmaailii <hamed.esmaili.1375@gmail.com>
2024-08-27 11:48:58 +0330

    Work on personal-finance-app project

---
+++ personal-finance-app/src/components/AddTransactionForm.js
@@ -0,0 +1,51 @@
+import React, {useState, useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
+
+const AddTransactionForm = () => {
+    const [text, setText] = useState('');
+    const [amount, setAmount] = useState(0);
+    const {transactions} = useContext(GlobalContext);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000),
+            text,
+            amount: +amount
+        };
+        addTransaction(newTransaction);
+        setText('');
+        setAmount(0);
+    }
+
+    return (
+        <div>
+            <h3>Add New Transaction</h3>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label htmlFor="text">Text</label>
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder="Enter text..."
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="amount">Amount</label>
+                    <input
+                        type="number"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        placeholder="Enter amount..."
+                        required
+                    />
+                </div>
+                <button className="btn">Add transaction</button>
+            </form>
+        </div>
+    );
+};
+
+export default AddTransactionForm;
